b95da546f43358caaf6eddbc73c77387a64ab7c8
Ajay Mallya <ajay.mallya@gmail.com>
2014-02-16 22:00:42 -0800

    Update with strong parameters.

--- app/controllers/catalog_items_controller.rb
+++ app/controllers/catalog_items_controller.rb
@@ -4,5 +4,24 @@ class CatalogItemsController < ApplicationController
   end
 
   def show
+    @catalog_item = CatalogItem.find(params[:id])
   end
+
+  def edit
+    @catalog_item = CatalogItem.find(params[:id])
+  end
+
+  def update
+    @catalog_item = CatalogItem.find(params[:id])
+    if @catalog_item.update(catalog_item_params)
+      redirect_to @catalog_item
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def catalog_item_params
+      params.require(:catalog_item).permit(:title, :authors, :description)
+    end
 end
