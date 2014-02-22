a730206adcf34fd56af9e927da7565eac28173a4
Ajay Mallya <ajay.mallya@gmail.com>
2014-02-21 20:05:31 -0800

    Added SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -63,4 +63,5 @@ WebApp::Application.routes.draw do
   match '/carts/:id/delete/:catalog_item_id', to: 'carts#delete_from_cart', as: :delete_from_cart, via: [:get, :post]
   resources :users
   get "/signin", to: 'sessions#new'
+  post "/signin", to: "sessions#create"
 end
