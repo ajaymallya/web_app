61ac78cba2c58c2d2ca8206bc2f8c9a616527462
Ajay Mallya <ajay.mallya@gmail.com>
2014-02-23 13:21:42 -0800

    Only display "Sing up" and "Sign in" links if user is not authenticated.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def signup_message
+    link_to "Sign up", new_user_path unless current_user
+  end
+
+  def signin_message
+    link_to "Sign in", signin_path unless current_user
+  end
+
+  private
+    def current_user
+      @current_user
+    end
 end
