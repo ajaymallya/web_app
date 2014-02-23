a00fb9e168f1cab55f2a6a32b421ecaacde966fa
Ajay Mallya <ajay.mallya@gmail.com>
2014-02-23 11:51:32 -0800

    Added greeting at top.

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -3,6 +3,10 @@ module CartsHelper
     link_to 'View Cart', current_user ? current_user.cart : '/signin'
   end
 
+  def greeting
+    "Hello, #{current_user.name}"
+  end
+
   private
     def current_user
       @current_user
